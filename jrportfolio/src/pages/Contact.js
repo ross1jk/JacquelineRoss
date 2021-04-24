a386dcfcf043970660912f470dfcd7a36641775a
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-24 18:21:07 -0400

    created grid compontent

--- jrportfolio/src/pages/Contact.js
+++ jrportfolio/src/pages/Contact.js
@@ -1,11 +1,18 @@
 import React from "react"; 
 import Textbox from "../components/Textbox";
+import { Row, Col } from "../components/Grid";
 
 function Contact() {
     return (
       <div>
+      <Row>
+      <Col size="6">
       <p> Placeholder Contact Page</p>
+      </Col>
+      <Col size="6">
       <Textbox />
+      </Col>
+      </Row>
       </div>
     );
   }
