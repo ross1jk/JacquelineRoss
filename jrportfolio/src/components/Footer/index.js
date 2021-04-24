d11924c51e976e83047cd690c5be8661813f8b64
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-23 22:19:54 -0400

    completed navbar and styling with links. imported font awesome. Need to add updated resume

--- jrportfolio/src/components/Footer/index.js
+++ jrportfolio/src/components/Footer/index.js
@@ -1,10 +1,23 @@
 // * A `Footer` component that appears on multiple pages
 import React from "react"; 
+import "./style.css"
 
 function Footer() {
     return (
-     <p>Footer Placeholder</p>
+      <div>
+      <footer className="navbar fixed-bottom">
+      <span>
+      <a href="./Documents/JacquelineRossResume.pdf"><i className="far fa-file"></i>My Resume</a> | 
+ <a href="https://github.com/ross1jk"><i className="fab fa-github"></i>GitHub</a> | 
+ <a href="https://www.linkedin.com/in/jacquelinekross/">Linked<i className="fab fa-linkedin-in"></i></a>
+      </span>
+      </footer>
+      </div>
+      
+     
     );
   }
   
   export default Footer;
+
+ 
--- jrportfolio/src/pages/About.js
+++ jrportfolio/src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
     return (
-      <p>Placeholder About Page</p>
+      <p>placeholder</p>
     );
   }
   
