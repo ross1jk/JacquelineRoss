b7beee5fbb03c30c422d4f6efc2e978c934bd07c
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-24 09:21:45 -0400

    added new pages and and components, blinking and routing complete

--- jrportfolio/src/pages/About.js
+++ jrportfolio/src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
     return (
-      <p>placeholder</p>
+      <p>Placeholder about me</p>
     );
   }
   
---
+++ jrportfolio/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+function Home() {
+    return (
+      <p>Placeholder Home Page</p>
+    );
+  }
+  
+  export default Home;
