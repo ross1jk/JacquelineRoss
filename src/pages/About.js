e7a8c5d67b5faa22f2c93bc227d19080e971468b
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-28 18:03:55 -0400

    updated readme and fixed spelling errors on about

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,10 +11,10 @@ function About() {
         </Col>
         <Col size="6">
          <Text large={"Hi, I'm Jacqueline"} />
-             <Text medium={"and I am a full stack web devleoper."} />
+             <Text medium={"and I am a full stack web developer."} />
              <Text small={"My Skillset: HTML, CSS, Bootstrap, JavaScript, jQuery, API, AJAX, Node, Handlebars, My SQL, Sequelize, MongoDB, Testing, Error Handling, React, Git, Express Servers, Heroku, and Progressive Web Applications"} />
              <Text small={"Currently, I am the Change Manager for Mainframe Services at Ford Motor Company. I lead all aspects of Change Management for the department, deliver essential communications both internally and externally, and participate in Department Projects."} />
-             <Text small={"I have organized and directed internal website migrations and consolidations. As well as managed externally facing contnent on our departments websites."} />
+             <Text small={"I have organized and directed internal website migrations and consolidations. As well as managed externally facing content on our department's websites."} />
         </Col>
 
       </Row>
