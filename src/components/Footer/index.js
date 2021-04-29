38b76f97b04f5685ad77bbc2aa4021cb5f7e4c53
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-28 20:54:36 -0400

    changed where resume is located

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
+import Resume from "../../pdf/JacquelineRossResume.pdf";
 
 function Footer() {
   return (
     <div>
       <footer className="navbar fixed-bottom" id="footer">
         <span>
-          <a href={process.env.PUBLIC_URL + "JacquelineRossResume.pdf"}><i className="far fa-file"></i>My Resume</a> |
+          <a href={(Resume)}><i className="far fa-file"></i>My Resume</a> |
           <a href="https://github.com/ross1jk"><i className="fab fa-github"></i>GitHub</a> |
           <a href="https://www.linkedin.com/in/jacquelinekross/">Linked<i className="fab fa-linkedin-in"></i></a>
         </span>
       </footer>
     </div>
-
 
   );
 }
